624b4308dd428b9cc50558e30a8625763cbd18f2
MYNAME <myemail@asdfasdfasdf.org>
2026-01-13 14:37:46 +0800

    ADD get method

--- index.js
+++ index.js
@@ -3,19 +3,28 @@ import bodyParser from "body-parser";
 import pg from "pg";
 const app = express();
 const port = 3000;
-const db = new.Client({
-  host:"localhost"
-  user:"postgres"
-  password:"123456"
-  database:"world"
+const db = new pg.Client({
+  host:"localhost",
+  user:"postgres",
+  password:"123456",
+  database:"world",
   port:5432
 })
 db.connect()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
+async function v() {
+  const result = await db.query("SELECT country_code FROM visited_country")
+  let countries = []
+  result.rows.forEach((countries)=>{
+    countries.push(countries.country_code)
+  })
+  return countries
+}
 app.get("/", async (req, res) => {
   //Write your code here.
+  const countries = await v()
+  res.render("index.ejs", {countries: countries, total: countries.length})
 });
 
 app.listen(port, () => {
