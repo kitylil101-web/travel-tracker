e212707e77cf455a7a5836bcc9f23d3a24330685
MYNAME <myemail@asdfasdfasdf.org>
2026-01-13 14:28:45 +0800

    add database

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
 const app = express();
 const port = 3000;
-
+const db = new.Client({
+  host:"localhost"
+  user:"postgres"
+  password:"123456"
+  database:"world"
+  port:5432
+})
+db.connect()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
